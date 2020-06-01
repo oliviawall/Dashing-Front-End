5a060a2ee9b6859163e08f90163c392adc7aac42
Olivia Wall <46054890+oliviawall@users.noreply.github.com>
2020-06-01 15:57:30 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,104 +1,66 @@
 import React, { Component } from 'react';
 import './styles.scss';
-
-import { auth, handleUserProfile } from './../../firebase/utils';
+import Buttons from './../forms/Button';
+import { signInWithGoogle, auth } from './../../firebase/utils';
 
 import FormInput from './../forms/FormInput';
 import Button from './../forms/Button';
 
 const initialState = {
-    displayName: '',
     email: '',
-    password: '',
-    confirmPassword: '',
-    errors: []
+    password: ''
 };
-
-class Signup extends Component {
+class SignIn extends Component {
     constructor(props) {
         super(props);
         this.state = {
             ...initialState
         };
+        
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e) {
         const { name, value } = e.target;
-
         this.setState({
             [name]: value
         });
     }
 
-    handleFormSubmit = async event => {
-        event.preventDefault();
-        const { displayName, email, password, confirmPassword } = this.state;
-
-        if (password !== confirmPassword) {
-            const err = ['Passwords Don\'t Match'];
-            this.setState({
-                errors: err
-            });
-            return;
-        }
+    handleSubmit = async e => {
+        e.preventDefault();
+        const { email, password } = this.state;
 
         try {
 
-            const { user } = await auth.createUserWithEmailAndPassword(email, password);
-
-            await handleUserProfile(user, { displayName });
-
+            await auth.signInWithEmailAndPassword(email, password);
             this.setState({
                 ...initialState
             });
-            
 
-        } catch(err) {
-            //console log(err);
-        }
-
+        } catch (err) {
+            // console.log(err);
+      }
     }
 
     render() {
-        const { displayName, email, password, confirmPassword, errors } = this.state;
-
+        const { email, password } = this.state;
         return (
-            <div className='signup'>
+            <div className='signin'>
                 <div className='wrap'>
                     <h2>
-                        Signup
-                    </h2>
-
-                    {errors.length > 0 && (
-                        <ul>
-                            {errors.map((err, index) => {
-                                return (
-                                    <li key={index}>
-                                        {err}
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    )}
+                        LogIn
+                </h2>
 
                     <div className='formWrap'>
-                        <form onSubmit={this.handleFormSubmit}>
-
-                            <FormInput
-                                type='text'
-                                name='displayName'
-                                value={displayName}
-                                placeholder='Full Name'
-                                onChange={this.handleChange}
-                            />
+                        <form onSubmit={this.handleSubmit}>
 
                             <FormInput
                                 type='email'
                                 name='email'
                                 value={email}
                                 placeholder='Email'
-                                onChange={this.handleChange}
+                                handleChange={this.handleChange}
                             />
 
                             <FormInput
@@ -106,20 +68,20 @@ class Signup extends Component {
                                 name='password'
                                 value={password}
                                 placeholder='Password'
-                                onChange={this.handleChange}
+                                handleChange={this.handleChange}
                             />
 
-                            <FormInput
-                                type='password'
-                                name='confirmPassword'
-                                value={confirmPassword}
-                                placeholder='Confirm Password'
-                                onChange={this.handleChange}
-                            />
+                            <Buttons type='submit'>
+                                LogIn
+                                </Buttons>
 
-                            <Button type='submit'>
-                                Register
-                        </Button>
+                            <div className='socialSignin'>
+                                <div className='row'>
+                                    <Buttons onClick={signInWithGoogle}>
+                                        Sign in with Google
+                                </Buttons>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -128,4 +90,4 @@ class Signup extends Component {
     }
 }
 
-export default Signup;
+export default SignIn;

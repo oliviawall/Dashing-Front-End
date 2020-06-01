7159706c6368ad6b1e527dc6d60d0940ac429e6c
Olivia Wall <46054890+oliviawall@users.noreply.github.com>
2020-06-01 15:55:38 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,12 @@
 import React from 'react';
 import './styles.scss';
-import { Link } from 'react-router-dom';
-import { auth } from './../../firebase/utils';
 
-import Logo from './../../assets/logo.png';
-
-const Header = props => {
-    const { currentUser } = props;
+const Button = ({ children, ...otherProps }) => {
     return (
-        <header className='header'>
-            <div className='wrap'>
-                <div className='logo'>
-                    <Link to='/'>
-                        <img src={Logo} alt='Dashing LOGO' />
-                    </Link>
-                </div>
-
-                <div className='callToActions'>
-
-                    {currentUser && (
-                        <ul>
-                            <li>
-                                <span onClick ={() => auth.signOut()}>
-                                    LogOut
-                                </span>
-                            </li>
-                        </ul>
-                    )} 
-                    {!currentUser && (
-                        <ul>
-                            <li>
-                                <Link to='/registration'>
-                                    Register
-                            </Link>
-                            </li>
-                            <li>
-                                <Link to='/login'>
-                                    Login
-                            </Link>
-                            </li>
-                        </ul>
-                    )}
-
-                </div>
-            </div>
-        </header>
+        <button className='btn' {...otherProps}>
+            {children}
+        </button>
     );
-};
+}
 
-Header.defaultProps = {
-    currentUser: null
-};
-
-export default Header;
+export default Button
